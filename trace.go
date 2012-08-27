7688c4d1d570b5bb8edd059f2d6a026527770458
Sam Burnett <srburnet@gmail.com>
2012-08-27 04:38:06 +0000

    format

--- trace.go
+++ trace.go
@@ -1,239 +1,240 @@
 package bismarkpassive
 
 import (
-    "bytes"
-    "fmt"
-    "io"
-    "io/ioutil"
-    "strconv"
-    "strings"
+	"bytes"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"strconv"
+	"strings"
 )
 
 type Section int
+
 const (
-    SectionIntro Section = iota
-    SectionWhitelist
-    SectionAnonymization
-    SectionPacketSeries
-    SectionFlowTable
-    SectionDnsTableA
-    SectionDnsTableCname
-    SectionAddressTable
-    SectionDropStatistics
+	SectionIntro Section = iota
+	SectionWhitelist
+	SectionAnonymization
+	SectionPacketSeries
+	SectionFlowTable
+	SectionDnsTableA
+	SectionDnsTableCname
+	SectionAddressTable
+	SectionDropStatistics
 )
 
 func (s Section) String() string {
-    if s == SectionIntro {
-        return "intro"
-    } else if s == SectionWhitelist {
-        return "whitelist"
-    } else if s == SectionAnonymization {
-        return "anonymization"
-    } else if s == SectionPacketSeries {
-        return "packet series"
-    } else if s == SectionFlowTable {
-        return "flow table"
-    } else if s == SectionDnsTableA {
-        return "DNS A records"
-    } else if s == SectionDnsTableCname {
-        return "DNS CNAME records"
-    } else if s == SectionAddressTable {
-        return "MAC addresses"
-    } else if s == SectionDropStatistics {
-        return "drop statistics"
-    }
-    return "unknown"
+	if s == SectionIntro {
+		return "intro"
+	} else if s == SectionWhitelist {
+		return "whitelist"
+	} else if s == SectionAnonymization {
+		return "anonymization"
+	} else if s == SectionPacketSeries {
+		return "packet series"
+	} else if s == SectionFlowTable {
+		return "flow table"
+	} else if s == SectionDnsTableA {
+		return "DNS A records"
+	} else if s == SectionDnsTableCname {
+		return "DNS CNAME records"
+	} else if s == SectionAddressTable {
+		return "MAC addresses"
+	} else if s == SectionDropStatistics {
+		return "drop statistics"
+	}
+	return "unknown"
 }
 
 type TraceParseError struct {
-    Section Section
-    Suberror error
+	Section  Section
+	Suberror error
 }
 
 func (e *TraceParseError) Error() string {
-    return fmt.Sprintf("Section %s %s", e.Section, e.Suberror)
+	return fmt.Sprintf("Section %s %s", e.Section, e.Suberror)
 }
 
 func newTraceParseError(section Section, suberror error) error {
-    return &TraceParseError{section, suberror}
+	return &TraceParseError{section, suberror}
 }
 
 type sectionError struct {
-    Message string
-    Suberror error
+	Message  string
+	Suberror error
 }
 
 func (e *sectionError) Error() string {
-    if e.Suberror == nil {
-        return e.Message
-    }
-    return fmt.Sprintf("%s: %s", e.Message, e.Suberror)
+	if e.Suberror == nil {
+		return e.Message
+	}
+	return fmt.Sprintf("%s: %s", e.Message, e.Suberror)
 }
 
 func newSectionError(message string, suberror error) error {
-    return &sectionError{message, suberror};
+	return &sectionError{message, suberror}
 }
 
-func linesToSections(lines [][]byte) ([][]string) {
-    sections := [][]string{}
-    currentSection := []string{}
-    for _, line := range(lines) {
-        if len(line) == 0 {
-            sections = append(sections, currentSection)
-            currentSection = []string{}
-        } else {
-            currentSection = append(currentSection, string(line))
-        }
-    }
-    if len(currentSection) > 0 {
-        sections = append(sections, currentSection)
-    }
-    return sections
+func linesToSections(lines [][]byte) [][]string {
+	sections := [][]string{}
+	currentSection := []string{}
+	for _, line := range lines {
+		if len(line) == 0 {
+			sections = append(sections, currentSection)
+			currentSection = []string{}
+		} else {
+			currentSection = append(currentSection, string(line))
+		}
+	}
+	if len(currentSection) > 0 {
+		sections = append(sections, currentSection)
+	}
+	return sections
 }
 
 func atoi32(s string) (int32, error) {
-    parsed, err := strconv.ParseInt(s, 0, 32)
-    if err != nil {
-        return 0, err
-    }
-    return int32(parsed), nil
+	parsed, err := strconv.ParseInt(s, 0, 32)
+	if err != nil {
+		return 0, err
+	}
+	return int32(parsed), nil
 }
 
 func atou32(s string) (uint32, error) {
-    parsed, err := strconv.ParseUint(s, 0, 32)
-    if err != nil {
-        return 0, err
-    }
-    return uint32(parsed), nil
+	parsed, err := strconv.ParseUint(s, 0, 32)
+	if err != nil {
+		return 0, err
+	}
+	return uint32(parsed), nil
 }
 
 func atoi64(s string) (int64, error) {
-    parsed, err := strconv.ParseInt(s, 0, 64)
-    if err != nil {
-        return 0, err
-    }
-    return int64(parsed), nil
+	parsed, err := strconv.ParseInt(s, 0, 64)
+	if err != nil {
+		return 0, err
+	}
+	return int64(parsed), nil
 }
 
 func words(s string) []string {
-    return strings.Split(s, " ");
+	return strings.Split(s, " ")
 }
 
 func parseSectionIntro(section []string, trace *Trace) error {
-    if len(section) < 1 {
-        return newSectionError("missing first line", nil)
-    }
-    firstLineWords := words(section[0])
-    if len(firstLineWords) < 1 {
-        return newSectionError("missing file format version", nil)
-    }
-    if fileFormatVersion, err := atoi32(firstLineWords[0]); err != nil {
-        return newSectionError("has invalid file format version", err)
-    } else {
-        trace.FileFormatVersion = &fileFormatVersion
-    }
+	if len(section) < 1 {
+		return newSectionError("missing first line", nil)
+	}
+	firstLineWords := words(section[0])
+	if len(firstLineWords) < 1 {
+		return newSectionError("missing file format version", nil)
+	}
+	if fileFormatVersion, err := atoi32(firstLineWords[0]); err != nil {
+		return newSectionError("has invalid file format version", err)
+	} else {
+		trace.FileFormatVersion = &fileFormatVersion
+	}
 
-    if len(section) < 2 {
-        return newSectionError("missing second line", nil)
-    }
-    secondLineWords := words(section[1])
-    if len(secondLineWords) < 1 {
-        return newSectionError("missing build id", nil)
-    }
-    trace.BuildId = &secondLineWords[0]
+	if len(section) < 2 {
+		return newSectionError("missing second line", nil)
+	}
+	secondLineWords := words(section[1])
+	if len(secondLineWords) < 1 {
+		return newSectionError("missing build id", nil)
+	}
+	trace.BuildId = &secondLineWords[0]
 
-    if len(section) < 3 {
-        return newSectionError("missing third line", nil)
-    }
-    thirdLineWords := words(section[2])
-    if len(thirdLineWords) < 1 {
-        return newSectionError("missing node id", nil)
-    } else if len(thirdLineWords) < 2 {
-        return newSectionError("missing process start time", nil)
-    } else if len(thirdLineWords) < 3 {
-        return newSectionError("missing sequence number", nil)
-    } else if len(thirdLineWords) < 4 {
-        return newSectionError("missing trace creation timestamp", nil)
-    }
-    trace.NodeId = &thirdLineWords[0]
-    if processStartTimeMicroseconds, err := atoi64(thirdLineWords[1]); err != nil {
-        return newSectionError("has invalid process start time", err)
-    } else {
-        trace.ProcessStartTimeMicroseconds = &processStartTimeMicroseconds
-    }
-    if sequenceNumber, err := atoi32(thirdLineWords[2]); err != nil {
-        return newSectionError("has invalid sequence number", err)
-    } else {
-        trace.SequenceNumber = &sequenceNumber
-    }
-    if traceCreationTimestamp, err := atoi64(thirdLineWords[3]); err != nil {
-        return newSectionError("has invalid trace creation tiemstamp", err)
-    } else {
-        trace.TraceCreationTimestamp = &traceCreationTimestamp
-    }
+	if len(section) < 3 {
+		return newSectionError("missing third line", nil)
+	}
+	thirdLineWords := words(section[2])
+	if len(thirdLineWords) < 1 {
+		return newSectionError("missing node id", nil)
+	} else if len(thirdLineWords) < 2 {
+		return newSectionError("missing process start time", nil)
+	} else if len(thirdLineWords) < 3 {
+		return newSectionError("missing sequence number", nil)
+	} else if len(thirdLineWords) < 4 {
+		return newSectionError("missing trace creation timestamp", nil)
+	}
+	trace.NodeId = &thirdLineWords[0]
+	if processStartTimeMicroseconds, err := atoi64(thirdLineWords[1]); err != nil {
+		return newSectionError("has invalid process start time", err)
+	} else {
+		trace.ProcessStartTimeMicroseconds = &processStartTimeMicroseconds
+	}
+	if sequenceNumber, err := atoi32(thirdLineWords[2]); err != nil {
+		return newSectionError("has invalid sequence number", err)
+	} else {
+		trace.SequenceNumber = &sequenceNumber
+	}
+	if traceCreationTimestamp, err := atoi64(thirdLineWords[3]); err != nil {
+		return newSectionError("has invalid trace creation tiemstamp", err)
+	} else {
+		trace.TraceCreationTimestamp = &traceCreationTimestamp
+	}
 
-    if len(section) < 4 {
-        // Missing PCAP statistics is ok, for compatibility.
-        return nil
-    }
-    fourthLineWords := words(section[3])
-    if len(fourthLineWords) < 1 {
-        return newSectionError("missing PCAP received", nil)
-    } else if len(fourthLineWords) < 2 {
-        return newSectionError("missing PCAP dropped", nil)
-    } else if len(fourthLineWords) < 3 {
-        return newSectionError("missing interface dropped", nil)
-    }
-    if pcapReceived, err := atou32(fourthLineWords[0]); err != nil {
-        return newSectionError("invalid PCAP received", err)
-    } else {
-        trace.PcapReceived = &pcapReceived
-    }
-    if pcapDropped, err := atou32(fourthLineWords[1]); err != nil {
-        return newSectionError("invalid PCAP dropped", err)
-    } else {
-        trace.PcapDropped = &pcapDropped
-    }
-    if interfaceDropped, err := atou32(fourthLineWords[2]); err != nil {
-        return newSectionError("invalid interface dropped", err)
-    } else {
-        trace.InterfaceDropped = &interfaceDropped
-    }
+	if len(section) < 4 {
+		// Missing PCAP statistics is ok, for compatibility.
+		return nil
+	}
+	fourthLineWords := words(section[3])
+	if len(fourthLineWords) < 1 {
+		return newSectionError("missing PCAP received", nil)
+	} else if len(fourthLineWords) < 2 {
+		return newSectionError("missing PCAP dropped", nil)
+	} else if len(fourthLineWords) < 3 {
+		return newSectionError("missing interface dropped", nil)
+	}
+	if pcapReceived, err := atou32(fourthLineWords[0]); err != nil {
+		return newSectionError("invalid PCAP received", err)
+	} else {
+		trace.PcapReceived = &pcapReceived
+	}
+	if pcapDropped, err := atou32(fourthLineWords[1]); err != nil {
+		return newSectionError("invalid PCAP dropped", err)
+	} else {
+		trace.PcapDropped = &pcapDropped
+	}
+	if interfaceDropped, err := atou32(fourthLineWords[2]); err != nil {
+		return newSectionError("invalid interface dropped", err)
+	} else {
+		trace.InterfaceDropped = &interfaceDropped
+	}
 
-    return nil
+	return nil
 }
 
 func makeTraceFromSections(sections [][]string) (*Trace, error) {
-    trace := new(Trace)
+	trace := new(Trace)
 
-    type sectionParser func([]string, *Trace) error
-    sectionsMap := map[Section] sectionParser {
-        SectionIntro: parseSectionIntro,
-    }
+	type sectionParser func([]string, *Trace) error
+	sectionsMap := map[Section]sectionParser{
+		SectionIntro: parseSectionIntro,
+	}
 
-    for section, parse := range sectionsMap {
-        if len(sections) <= int(section) {
-            return nil, newTraceParseError(section, newSectionError("missing", nil))
-        }
-        sectionErr := parse(sections[int(section)], trace)
-        if sectionErr != nil {
-            return nil, newTraceParseError(section, sectionErr)
-        }
-    }
+	for section, parse := range sectionsMap {
+		if len(sections) <= int(section) {
+			return nil, newTraceParseError(section, newSectionError("missing", nil))
+		}
+		sectionErr := parse(sections[int(section)], trace)
+		if sectionErr != nil {
+			return nil, newTraceParseError(section, sectionErr)
+		}
+	}
 
-    return trace, nil
+	return trace, nil
 }
 
 func ParseTrace(source io.Reader) (*Trace, error) {
-    contents, err := ioutil.ReadAll(source)
-    if err != nil {
-        return nil, err
-    }
-    lines := bytes.Split(contents, []byte{'\n'})
-    sections := linesToSections(lines)
-    trace, err := makeTraceFromSections(sections)
-    if err != nil {
-        return nil, err
-    }
-    return trace, nil
+	contents, err := ioutil.ReadAll(source)
+	if err != nil {
+		return nil, err
+	}
+	lines := bytes.Split(contents, []byte{'\n'})
+	sections := linesToSections(lines)
+	trace, err := makeTraceFromSections(sections)
+	if err != nil {
+		return nil, err
+	}
+	return trace, nil
 }
--- trace_test.go
+++ trace_test.go
@@ -1,169 +1,169 @@
 package bismarkpassive
 
 import (
-    "code.google.com/p/goprotobuf/proto"
-    "fmt"
-    "testing"
+	"code.google.com/p/goprotobuf/proto"
+	"fmt"
+	"testing"
 )
 
 func printSections(sections [][]string) {
-    fmt.Println("{")
-    for _, section := range sections {
-        fmt.Println(" {")
-        for _, line := range section {
-            fmt.Printf("  %s\n", line)
-        }
-        fmt.Println(" }")
-    }
-    fmt.Println("}")
+	fmt.Println("{")
+	for _, section := range sections {
+		fmt.Println(" {")
+		for _, line := range section {
+			fmt.Printf("  %s\n", line)
+		}
+		fmt.Println(" }")
+	}
+	fmt.Println("}")
 }
 
 func ExampleLinesToSections_simple() {
-    lines := [][]byte {
-        []byte("hello"),
-        []byte("world"),
-        []byte(""),
-        []byte("test"),
-    }
-    printSections(linesToSections(lines))
+	lines := [][]byte{
+		[]byte("hello"),
+		[]byte("world"),
+		[]byte(""),
+		[]byte("test"),
+	}
+	printSections(linesToSections(lines))
 
-    // Output:
-    // {
-    //  {
-    //   hello
-    //   world
-    //  }
-    //  {
-    //   test
-    //  }
-    // }
+	// Output:
+	// {
+	//  {
+	//   hello
+	//   world
+	//  }
+	//  {
+	//   test
+	//  }
+	// }
 }
 
 func ExampleLinesToSections_empty() {
-    lines := [][]byte {
-        []byte("hello"),
-        []byte("world"),
-        []byte(""),
-        []byte(""),
-        []byte("test"),
-    }
-    printSections(linesToSections(lines))
+	lines := [][]byte{
+		[]byte("hello"),
+		[]byte("world"),
+		[]byte(""),
+		[]byte(""),
+		[]byte("test"),
+	}
+	printSections(linesToSections(lines))
 
-    // Output:
-    // {
-    //  {
-    //   hello
-    //   world
-    //  }
-    //  {
-    //  }
-    //  {
-    //   test
-    //  }
-    // }
+	// Output:
+	// {
+	//  {
+	//   hello
+	//   world
+	//  }
+	//  {
+	//  }
+	//  {
+	//   test
+	//  }
+	// }
 }
 
 func ExampleLinesToSections_trim() {
-    lines := [][]byte {
-        []byte("hello"),
-        []byte("world"),
-        []byte(""),
-    }
-    printSections(linesToSections(lines))
+	lines := [][]byte{
+		[]byte("hello"),
+		[]byte("world"),
+		[]byte(""),
+	}
+	printSections(linesToSections(lines))
 
-    // Output:
-    // {
-    //  {
-    //   hello
-    //   world
-    //  }
-    // }
+	// Output:
+	// {
+	//  {
+	//   hello
+	//   world
+	//  }
+	// }
 }
 
 func checkForSectionError(t *testing.T, lines []string) {
-    trace := new(Trace)
-    err := parseSectionIntro(lines, trace)
-    if err == nil {
-        t.Fatal("Trace should be invalid:", lines)
-    }
-    if e, ok := err.(*sectionError); !ok {
-        t.Fatal("Should return sectionError. Instead got", e)
-    }
+	trace := new(Trace)
+	err := parseSectionIntro(lines, trace)
+	if err == nil {
+		t.Fatal("Trace should be invalid:", lines)
+	}
+	if e, ok := err.(*sectionError); !ok {
+		t.Fatal("Should return sectionError. Instead got", e)
+	}
 }
 
 func TestParseSectionIntro_Invalid(t *testing.T) {
-    // Incompleteness
-    checkForSectionError(t, []string {})
-    checkForSectionError(t, []string {""})
-    checkForSectionError(t, []string {"10"})
-    checkForSectionError(t, []string {"10", ""})
-    checkForSectionError(t, []string {"10", "BUILDID"})
-    checkForSectionError(t, []string {"10", "BUILDID", ""})
-    checkForSectionError(t, []string {"10", "BUILDID", "NODEID"})
-    checkForSectionError(t, []string {"10", "BUILDID", "NODEID 123"})
-    checkForSectionError(t, []string {"10", "BUILDID", "NODEID 123 321"})
-    checkForSectionError(t, []string {"10", "BUILDID", "NODEID 123 321 789", ""})
-    checkForSectionError(t, []string {"10", "BUILDID", "NODEID 123 321 789", "76"})
-    checkForSectionError(t, []string {"10", "BUILDID", "NODEID 123 321 789", "76 85"})
+	// Incompleteness
+	checkForSectionError(t, []string{})
+	checkForSectionError(t, []string{""})
+	checkForSectionError(t, []string{"10"})
+	checkForSectionError(t, []string{"10", ""})
+	checkForSectionError(t, []string{"10", "BUILDID"})
+	checkForSectionError(t, []string{"10", "BUILDID", ""})
+	checkForSectionError(t, []string{"10", "BUILDID", "NODEID"})
+	checkForSectionError(t, []string{"10", "BUILDID", "NODEID 123"})
+	checkForSectionError(t, []string{"10", "BUILDID", "NODEID 123 321"})
+	checkForSectionError(t, []string{"10", "BUILDID", "NODEID 123 321 789", ""})
+	checkForSectionError(t, []string{"10", "BUILDID", "NODEID 123 321 789", "76"})
+	checkForSectionError(t, []string{"10", "BUILDID", "NODEID 123 321 789", "76 85"})
 
-    // Integer conversion
-    checkForSectionError(t, []string {"STR", "BUILDID", "NODEID 123 321 789", "76 85 99"})
-    checkForSectionError(t, []string {"10", "BUILDID", "NODEID STR 321 789", "76 85 99"})
-    checkForSectionError(t, []string {"10", "BUILDID", "NODEID 123 STR 789", "76 85 99"})
-    checkForSectionError(t, []string {"10", "BUILDID", "NODEID 123 321 STR", "76 85 99"})
-    checkForSectionError(t, []string {"10", "BUILDID", "NODEID 123 321 789", "STR 85 99"})
-    checkForSectionError(t, []string {"10", "BUILDID", "NODEID 123 321 789", "76 STR 99"})
-    checkForSectionError(t, []string {"10", "BUILDID", "NODEID 123 321 789", "76 85 STR"})
+	// Integer conversion
+	checkForSectionError(t, []string{"STR", "BUILDID", "NODEID 123 321 789", "76 85 99"})
+	checkForSectionError(t, []string{"10", "BUILDID", "NODEID STR 321 789", "76 85 99"})
+	checkForSectionError(t, []string{"10", "BUILDID", "NODEID 123 STR 789", "76 85 99"})
+	checkForSectionError(t, []string{"10", "BUILDID", "NODEID 123 321 STR", "76 85 99"})
+	checkForSectionError(t, []string{"10", "BUILDID", "NODEID 123 321 789", "STR 85 99"})
+	checkForSectionError(t, []string{"10", "BUILDID", "NODEID 123 321 789", "76 STR 99"})
+	checkForSectionError(t, []string{"10", "BUILDID", "NODEID 123 321 789", "76 85 STR"})
 }
 
 func TestParseSectionIntro_Valid(t *testing.T) {
-    lines := []string {
-        "10",
-        "BUILDID",
-        "NODEID 123 321 789",
-        "12 23 34",
-    }
-    trace := Trace{}
-    err := parseSectionIntro(lines, &trace)
-    if err != nil {
-        t.Fatal("Unexpected error:", err)
-    }
-    expectedTrace := Trace{
-        FileFormatVersion: proto.Int32(10),
-        BuildId: proto.String("BUILDID"),
-        NodeId: proto.String("NODEID"),
-        ProcessStartTimeMicroseconds: proto.Int64(123),
-        SequenceNumber: proto.Int32(321),
-        TraceCreationTimestamp: proto.Int64(789),
-        PcapReceived: proto.Uint32(12),
-        PcapDropped: proto.Uint32(23),
-        InterfaceDropped: proto.Uint32(34),
-    }
-    if !proto.Equal(&expectedTrace, &trace) {
-        t.Fatalf("Protocol buffers not equal")
-    }
+	lines := []string{
+		"10",
+		"BUILDID",
+		"NODEID 123 321 789",
+		"12 23 34",
+	}
+	trace := Trace{}
+	err := parseSectionIntro(lines, &trace)
+	if err != nil {
+		t.Fatal("Unexpected error:", err)
+	}
+	expectedTrace := Trace{
+		FileFormatVersion: proto.Int32(10),
+		BuildId:           proto.String("BUILDID"),
+		NodeId:            proto.String("NODEID"),
+		ProcessStartTimeMicroseconds: proto.Int64(123),
+		SequenceNumber:               proto.Int32(321),
+		TraceCreationTimestamp:       proto.Int64(789),
+		PcapReceived:                 proto.Uint32(12),
+		PcapDropped:                  proto.Uint32(23),
+		InterfaceDropped:             proto.Uint32(34),
+	}
+	if !proto.Equal(&expectedTrace, &trace) {
+		t.Fatalf("Protocol buffers not equal")
+	}
 }
 
 func TestParseSectionIntro_ValidOptional(t *testing.T) {
-    lines := []string {
-        "10",
-        "BUILDID",
-        "NODEID 123 321 789",
-    }
-    trace := Trace{}
-    err := parseSectionIntro(lines, &trace)
-    if err != nil {
-        t.Fatal("Unexpected error:", err)
-    }
-    expectedTrace := Trace{
-        FileFormatVersion: proto.Int32(10),
-        BuildId: proto.String("BUILDID"),
-        NodeId: proto.String("NODEID"),
-        ProcessStartTimeMicroseconds: proto.Int64(123),
-        SequenceNumber: proto.Int32(321),
-        TraceCreationTimestamp: proto.Int64(789),
-    }
-    if !proto.Equal(&expectedTrace, &trace) {
-        t.Fatal("Protocol buffers not equal")
-    }
+	lines := []string{
+		"10",
+		"BUILDID",
+		"NODEID 123 321 789",
+	}
+	trace := Trace{}
+	err := parseSectionIntro(lines, &trace)
+	if err != nil {
+		t.Fatal("Unexpected error:", err)
+	}
+	expectedTrace := Trace{
+		FileFormatVersion: proto.Int32(10),
+		BuildId:           proto.String("BUILDID"),
+		NodeId:            proto.String("NODEID"),
+		ProcessStartTimeMicroseconds: proto.Int64(123),
+		SequenceNumber:               proto.Int32(321),
+		TraceCreationTimestamp:       proto.Int64(789),
+	}
+	if !proto.Equal(&expectedTrace, &trace) {
+		t.Fatal("Protocol buffers not equal")
+	}
 }
