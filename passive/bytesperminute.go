589c73c54e45670d782e98697bdaa081fec5e0f4
Sam Burnett <srburnet@gmail.com>
2013-01-22 17:26:50 +0000

    Use new pipeline testing functions.

--- passive/bytesperminute.go
+++ passive/bytesperminute.go
@@ -1,83 +1,91 @@
 package passive
 
-//import (
-//	"bytes"
-//	"code.google.com/p/goprotobuf/proto"
-//	"log"
-//	"time"
-//)
-//
-//func BytesPerMinuteMapper(inputChan, outputChan chan *LevelDbRecord) {
-//	nonce := int64(0)
-//	for record := range inputChan {
-//		key := parseKey(record.Key)
-//		if len(key) != 5 {
-//			log.Fatalf("Invalid length for key %q", record.Key)
-//		}
-//
-//		trace := Trace{}
-//		if err := proto.Unmarshal(record.Value, &trace); err != nil {
-//			log.Fatalf("Error ummarshaling protocol buffer: %v", err)
-//		}
-//
-//		buckets := make(map[int64]int64)
-//		for _, packetSeriesEntry := range trace.PacketSeries {
-//			timestamp := time.Unix(0, *packetSeriesEntry.TimestampMicroseconds*1000)
-//			minuteTimestamp := time.Date(timestamp.Year(), timestamp.Month(), timestamp.Day(), timestamp.Hour(), timestamp.Minute(), 0, 0, time.UTC)
-//			buckets[minuteTimestamp.Unix()] += int64(*packetSeriesEntry.Size)
-//		}
-//
-//		for timestamp, size := range buckets {
-//			timestampBytes, err := encodeLexicographicInt64(timestamp)
-//			if err != nil {
-//				log.Printf("Error encoding timestamp: %v", err)
-//				continue
-//			}
-//			nonceBytes, err := encodeLexicographicInt64(nonce)
-//			if err != nil {
-//				log.Fatalf("Error encoding nonce: %v", err)
-//			}
-//			nonce++
-//			outputChan <- &LevelDbRecord{
-//				Key:   makeKey("bytes_per_minute_mapped", key[1], timestampBytes, nonceBytes),
-//				Value: encodeInt64(size),
-//			}
-//		}
-//	}
-//}
-//
-//func BytesPerMinuteReducer(inputChan, outputChan chan *LevelDbRecord) {
-//	var currentSize int64
-//	var currentNode, currentTimestamp []byte
-//	for record := range inputChan {
-//		key := parseKey(record.Key)
-//		if len(key) != 4 {
-//			log.Fatalf("Invalid length for key %q", record.Key)
-//		}
-//		if currentNode == nil || currentTimestamp == nil {
-//			currentNode = key[1]
-//			currentTimestamp = key[2]
-//		}
-//		if !bytes.Equal(key[1], currentNode) || !bytes.Equal(key[2], currentTimestamp) {
-//			outputChan <- &LevelDbRecord{
-//				Key:   makeKey("bytes_per_minute", currentNode, currentTimestamp),
-//				Value: encodeInt64(currentSize),
-//			}
-//			currentNode = key[1]
-//			currentTimestamp = key[2]
-//			currentSize = 0
-//		}
-//
-//		value, err := decodeInt64(record.Value)
-//		if err != nil {
-//			log.Fatal("Error decoding size from VarInt: %v", err)
-//		}
-//		currentSize += value
-//	}
-//	if currentNode != nil && currentTimestamp != nil {
-//		outputChan <- &LevelDbRecord{
-//			Key:   makeKey("bytes_per_minute", currentNode, currentTimestamp),
-//			Value: encodeInt64(currentSize),
-//		}
-//	}
-//}
+import (
+	"bytes"
+	"code.google.com/p/goprotobuf/proto"
+	"github.com/sburnett/transformer"
+	"github.com/sburnett/transformer/key"
+	"log"
+	"time"
+)
+
+func BytesPerMinutePipeline(workers int) []transformer.PipelineStage {
+	return []transformer.PipelineStage{
+		transformer.PipelineStage{
+			Name:        "BytesPerMinuteMapper",
+			Transformer: transformer.MakeDoTransformer(BytesPerMinuteMapper(transformer.NewNonce()), workers),
+			InputDbs:    []string{"traces"},
+			OutputDbs:   []string{"bytesperminute-mapped"},
+			OnlyKeys:    false,
+		},
+		transformer.PipelineStage{
+			Name:        "BytesPerMinuteReducer",
+			Transformer: transformer.TransformFunc(BytesPerMinuteReducer),
+			InputDbs:    []string{"bytesperminute-mapped"},
+			OutputDbs:   []string{"bytesperminute"},
+			OnlyKeys:    false,
+		},
+	}
+}
+
+type BytesPerMinuteMapper transformer.Nonce
+
+func (nonce BytesPerMinuteMapper) Do(inputRecord *transformer.LevelDbRecord, outputChan chan *transformer.LevelDbRecord) {
+	traceKey := DecodeTraceKey(inputRecord.Key)
+
+	trace := Trace{}
+	if err := proto.Unmarshal(inputRecord.Value, &trace); err != nil {
+		log.Fatalf("Error ummarshaling protocol buffer: %v", err)
+	}
+
+	buckets := make(map[int64]int64)
+	for _, packetSeriesEntry := range trace.PacketSeries {
+		timestamp := time.Unix(0, *packetSeriesEntry.TimestampMicroseconds*1000)
+		minuteTimestamp := time.Date(timestamp.Year(), timestamp.Month(), timestamp.Day(), timestamp.Hour(), timestamp.Minute(), 0, 0, time.UTC)
+		buckets[minuteTimestamp.Unix()] += int64(*packetSeriesEntry.Size)
+	}
+
+	for timestamp, size := range buckets {
+		outputChan <- &transformer.LevelDbRecord{
+			Key:   key.EncodeOrDie(traceKey.NodeId, timestamp, transformer.Nonce(nonce).Get()),
+			Value: key.EncodeOrDie(size),
+		}
+	}
+}
+
+func BytesPerMinuteReducer(inputChan chan *transformer.LevelDbRecord, outputChans ...chan *transformer.LevelDbRecord) {
+	outputChan := outputChans[0]
+
+	var currentSize int64
+	var currentNode []byte
+	currentTimestamp := int64(-1)
+	for record := range inputChan {
+		var node []byte
+		var timestamp int64
+		key.DecodeOrDie(record.Key, &node, &timestamp)
+
+		if currentNode == nil || currentTimestamp < 0 {
+			currentNode = node
+			currentTimestamp = timestamp
+		}
+		if !bytes.Equal(node, currentNode) || timestamp != currentTimestamp {
+			outputChan <- &transformer.LevelDbRecord{
+				Key:   key.EncodeOrDie(currentNode, currentTimestamp),
+				Value: key.EncodeOrDie(currentSize),
+			}
+			currentNode = node
+			currentTimestamp = timestamp
+			currentSize = 0
+		}
+
+		var value int64
+		key.DecodeOrDie(record.Value, &value)
+		currentSize += value
+	}
+	if currentNode != nil && currentTimestamp >= 0 {
+		outputChan <- &transformer.LevelDbRecord{
+			Key:   key.EncodeOrDie(currentNode, currentTimestamp),
+			Value: key.EncodeOrDie(currentSize),
+		}
+	}
+}
--- passive/bytesperminute_test.go
+++ passive/bytesperminute_test.go
@@ -1,92 +1,144 @@
-package passive_test
+package passive
 
-//import (
-//	. "bismark/passive"
-//	"code.google.com/p/goprotobuf/proto"
-//	"fmt"
-//	"time"
-//)
-//
-//func makePacketSeriesEntry(timestamp int64, size int32) *PacketSeriesEntry {
-//	return &PacketSeriesEntry{
-//		TimestampMicroseconds: proto.Int64(timestamp),
-//		Size:                  proto.Int32(size),
-//	}
-//}
-//
-//func runBytesPerMinute(inputRecords []*LevelDbRecord, inputTable string) {
-//	mapperOutput := runTransform(BytesPerMinuteMapper, inputRecords, inputTable)
-//	reducerOutput := runTransform(BytesPerMinuteReducer, mapperOutput, "bytes_per_minute_mapped")
-//	for _, entry := range reducerOutput {
-//		fmt.Printf("%s: %v\n", entry.Key, decodeInt64(entry.Value))
-//	}
-//}
-//
-//func ExampleBytesPerMinute_simple() {
-//	trace1 := &Trace{}
-//	trace1.PacketSeries = make([]*PacketSeriesEntry, 2)
-//	trace1.PacketSeries[0] = makePacketSeriesEntry(10, 20)
-//	trace1.PacketSeries[1] = makePacketSeriesEntry(30, 40)
-//	trace2 := &Trace{}
-//	trace2.PacketSeries = make([]*PacketSeriesEntry, 2)
-//	trace2.PacketSeries[0] = makePacketSeriesEntry(50, 60)
-//	trace2.PacketSeries[1] = makePacketSeriesEntry(70, 80)
-//	records := []*LevelDbRecord{
-//		createLevelDbRecord("table:node0:anon0:session0:seq0", trace1),
-//		createLevelDbRecord("table:node0:anon0:session0:seq1", trace2),
-//	}
-//	runBytesPerMinute(records, "table")
-//
-//	// Output:
-//	// bytes_per_minute:node0:00000000000000000000: 200
-//}
-//
-//func ExampleBytesPerMinute_twoMinutes() {
-//	trace1 := &Trace{}
-//	trace1.PacketSeries = make([]*PacketSeriesEntry, 2)
-//	trace1.PacketSeries[0] = makePacketSeriesEntry(10*int64(time.Second/time.Microsecond), 20)
-//	trace1.PacketSeries[1] = makePacketSeriesEntry(30*int64(time.Second/time.Microsecond), 40)
-//	trace2 := &Trace{}
-//	trace2.PacketSeries = make([]*PacketSeriesEntry, 2)
-//	trace2.PacketSeries[0] = makePacketSeriesEntry(50*int64(time.Second/time.Microsecond), 60)
-//	trace2.PacketSeries[1] = makePacketSeriesEntry(70*int64(time.Second/time.Microsecond), 80)
-//	records := []*LevelDbRecord{
-//		createLevelDbRecord("table:node0:anon0:session0:seq0", trace1),
-//		createLevelDbRecord("table:node0:anon0:session0:seq1", trace2),
-//	}
-//	runBytesPerMinute(records, "table")
-//
-//	// Output:
-//	// bytes_per_minute:node0:00000000000000000000: 120
-//	// bytes_per_minute:node0:00000000000000000060: 80
-//}
-//
-//func ExampleBytesPerMinute_multipleSessions() {
-//	trace1 := &Trace{}
-//	trace1.PacketSeries = make([]*PacketSeriesEntry, 1)
-//	trace1.PacketSeries[0] = makePacketSeriesEntry(0, 20)
-//	records := []*LevelDbRecord{
-//		createLevelDbRecord("table:node0:anon0:session0:seq0", trace1),
-//		createLevelDbRecord("table:node0:anon0:session1:seq0", trace1),
-//		createLevelDbRecord("table:node0:anon1:session0:seq0", trace1),
-//	}
-//	runBytesPerMinute(records, "table")
-//
-//	// Output:
-//	// bytes_per_minute:node0:00000000000000000000: 60
-//}
-//
-//func ExampleBytesPerMinute_multipleNodes() {
-//	trace1 := &Trace{}
-//	trace1.PacketSeries = make([]*PacketSeriesEntry, 1)
-//	trace1.PacketSeries[0] = makePacketSeriesEntry(0, 20)
-//	records := []*LevelDbRecord{
-//		createLevelDbRecord("table:node0:anon0:session0:seq0", trace1),
-//		createLevelDbRecord("table:node1:anon0:session0:seq0", trace1),
-//	}
-//	runBytesPerMinute(records, "table")
-//
-//	// Output:
-//	// bytes_per_minute:node0:00000000000000000000: 20
-//	// bytes_per_minute:node1:00000000000000000000: 20
-//}
+import (
+	"bytes"
+	"code.google.com/p/goprotobuf/proto"
+	"github.com/sburnett/transformer"
+	"github.com/sburnett/transformer/key"
+	"sort"
+	"testing"
+	"time"
+)
+
+func makePacketSeriesEntry(timestamp int64, size int32) *PacketSeriesEntry {
+	return &PacketSeriesEntry{
+		TimestampMicroseconds: proto.Int64(timestamp),
+		Size:                  proto.Int32(size),
+	}
+}
+
+func runBytesPerMinutePipeline(t *testing.T, traces map[string]Trace, expectedOutput []transformer.LevelDbRecord) {
+	databases := make(map[string]map[string]string)
+	traceDatabase := make(map[string]string)
+	for encodedKey, trace := range traces {
+		encodedTrace, err := proto.Marshal(&trace)
+		if err != nil {
+			t.Fatalf("Error encoding protocol buffer: %v", err)
+		}
+		traceDatabase[encodedKey] = string(encodedTrace)
+	}
+	databases["traces"] = traceDatabase
+
+	transformer.RunPipelineWithoutLevelDb(BytesPerMinutePipeline(1), databases, 100)
+	database, ok := databases["bytesperminute"]
+	if !ok {
+		t.Fatalf("Missing expected output database")
+	}
+	actualOutput := make([]*transformer.LevelDbRecord, 0)
+	for k, v := range database {
+		actualOutput = append(actualOutput, &transformer.LevelDbRecord{Key: []byte(k), Value: []byte(v)})
+	}
+	sort.Sort(transformer.LevelDbRecordSlice(actualOutput))
+	for idx, expectedRecord := range expectedOutput {
+		if len(actualOutput) <= idx {
+			t.Fatalf("Missing expected record %s: %s", expectedRecord.Key, expectedRecord.Value)
+		}
+		actualRecord := actualOutput[idx]
+		if !bytes.Equal(expectedRecord.Key, actualRecord.Key) {
+			t.Fatalf("Expected key: %s, Got: %s", expectedRecord.Key, actualRecord.Key)
+		}
+		if !bytes.Equal(expectedRecord.Value, actualRecord.Value) {
+			t.Fatalf("Expected value: %s, Got: %s", expectedRecord.Value, actualRecord.Value)
+		}
+	}
+	for idx := len(expectedOutput); idx < len(actualOutput); idx++ {
+		t.Fatalf("Got extra record %s: %s", actualOutput[idx].Key, actualOutput[idx].Value)
+	}
+}
+
+func TestBytesPerMinute_simple(t *testing.T) {
+	trace1 := Trace{}
+	trace1.PacketSeries = make([]*PacketSeriesEntry, 2)
+	trace1.PacketSeries[0] = makePacketSeriesEntry(10, 20)
+	trace1.PacketSeries[1] = makePacketSeriesEntry(30, 40)
+	trace2 := Trace{}
+	trace2.PacketSeries = make([]*PacketSeriesEntry, 2)
+	trace2.PacketSeries[0] = makePacketSeriesEntry(50, 60)
+	trace2.PacketSeries[1] = makePacketSeriesEntry(70, 80)
+	records := map[string]Trace{
+		string(key.EncodeOrDie("node0", "anon0", "session0", int32(0))): trace1,
+		string(key.EncodeOrDie("node0", "anon0", "session0", int32(1))): trace2,
+	}
+	expectedOutput := []transformer.LevelDbRecord{
+		transformer.LevelDbRecord{
+			Key:   key.EncodeOrDie("node0", int64(0)),
+			Value: key.EncodeOrDie(int64(200)),
+		},
+	}
+	runBytesPerMinutePipeline(t, records, expectedOutput)
+}
+
+func TestBytesPerMinute_twoMinutes(t *testing.T) {
+	trace1 := Trace{}
+	trace1.PacketSeries = make([]*PacketSeriesEntry, 2)
+	trace1.PacketSeries[0] = makePacketSeriesEntry(10*int64(time.Second/time.Microsecond), 20)
+	trace1.PacketSeries[1] = makePacketSeriesEntry(30*int64(time.Second/time.Microsecond), 40)
+	trace2 := Trace{}
+	trace2.PacketSeries = make([]*PacketSeriesEntry, 2)
+	trace2.PacketSeries[0] = makePacketSeriesEntry(50*int64(time.Second/time.Microsecond), 60)
+	trace2.PacketSeries[1] = makePacketSeriesEntry(70*int64(time.Second/time.Microsecond), 80)
+	records := map[string]Trace{
+		string(key.EncodeOrDie("node0", "anon0", "session0", int32(0))): trace1,
+		string(key.EncodeOrDie("node0", "anon0", "session0", int32(1))): trace2,
+	}
+	expectedOutput := []transformer.LevelDbRecord{
+		transformer.LevelDbRecord{
+			Key:   key.EncodeOrDie("node0", int64(0)),
+			Value: key.EncodeOrDie(int64(120)),
+		},
+		transformer.LevelDbRecord{
+			Key:   key.EncodeOrDie("node0", int64(60)),
+			Value: key.EncodeOrDie(int64(80)),
+		},
+	}
+	runBytesPerMinutePipeline(t, records, expectedOutput)
+}
+
+func TestBytesPerMinute_multipleSessions(t *testing.T) {
+	trace1 := Trace{}
+	trace1.PacketSeries = make([]*PacketSeriesEntry, 1)
+	trace1.PacketSeries[0] = makePacketSeriesEntry(0, 20)
+	records := map[string]Trace{
+		string(key.EncodeOrDie("node0", "anon0", "session0", int32(0))): trace1,
+		string(key.EncodeOrDie("node0", "anon0", "session1", int32(0))): trace1,
+		string(key.EncodeOrDie("node0", "anon1", "session0", int32(0))): trace1,
+	}
+	expectedOutput := []transformer.LevelDbRecord{
+		transformer.LevelDbRecord{
+			Key:   key.EncodeOrDie("node0", int64(0)),
+			Value: key.EncodeOrDie(int64(60)),
+		},
+	}
+	runBytesPerMinutePipeline(t, records, expectedOutput)
+}
+
+func TestBytesPerMinute_multipleNodes(t *testing.T) {
+	trace1 := Trace{}
+	trace1.PacketSeries = make([]*PacketSeriesEntry, 1)
+	trace1.PacketSeries[0] = makePacketSeriesEntry(0, 20)
+	records := map[string]Trace{
+		string(key.EncodeOrDie("node0", "anon0", "session0", int32(0))): trace1,
+		string(key.EncodeOrDie("node1", "anon0", "session0", int32(0))): trace1,
+	}
+	expectedOutput := []transformer.LevelDbRecord{
+		transformer.LevelDbRecord{
+			Key:   key.EncodeOrDie("node0", int64(0)),
+			Value: key.EncodeOrDie(int64(20)),
+		},
+		transformer.LevelDbRecord{
+			Key:   key.EncodeOrDie("node1", int64(0)),
+			Value: key.EncodeOrDie(int64(20)),
+		},
+	}
+	runBytesPerMinutePipeline(t, records, expectedOutput)
+}
